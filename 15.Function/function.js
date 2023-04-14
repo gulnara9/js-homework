9fdcea3fcafce912dfa9f2b9e3a64a7ddc1c0113
gulnara <123542083+gulnara9@users.noreply.github.com>
2023-04-13 22:33:32 -0700

    task 3 editted
    another solution was written for the third task

--- 15.Function/function.js
+++ 15.Function/function.js
@@ -38,14 +38,16 @@ indexItem([1,5,8,9, "b", 3, 4, 7], "b");
 
 //or
 //function indexItem(arr, str) {
-  //for (i = 0; i < arr.length; i++) {
-   // if (arr[i] == str) {
-      //console.log(i);
-    //} else {
-    //  continue;
-   // }
-  //}
-//}we can write this type
+//  for (i = 0; i < arr.length; i++) {
+//    if (arr[i] == str) {
+//      return (i);
+//    } else {
+//      continue;
+//    }
+//  }
+//}
+//console.log(indexItem([1,5,8,9, "b", 3, 4, 7], "b"));
+//we can also write the third task in this way
 
 /////////////// 4.Return the Sum of Two Numbers.
 let addition = function (a, b) {

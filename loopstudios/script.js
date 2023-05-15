a779a4ecb49c77ca55201f1c3789f61229515a39
gulnara <123542083+gulnara9@users.noreply.github.com>
2023-05-15 08:55:28 +0400

    Add files via upload

---
+++ loopstudios/script.js
@@ -0,0 +1,11 @@
+const menuIcon = document.querySelector(".menu-icon");
+const nav = document.querySelector("#nav");
+const header = document.querySelector("header");
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
